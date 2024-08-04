06e8be5678bfde51c2e999d1404558f3d43722ed
vigbeamer <vignesh.shanmugavelu@getbeamer.com>
2024-08-04 13:01:43 +0530

    add generateParagraphs function

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const { LoremIpsum } = require("lorem-ipsum");
 
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4,
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4,
+  },
+});
+
 /**
  *
  * @param {number} n
  */
 
 const generateSentence = (n) => {
-  const lorem = new LoremIpsum({
-    sentencesPerParagraph: {
-      max: 8,
-      min: 4,
-    },
-    wordsPerSentence: {
-      max: 16,
-      min: 4,
-    },
-  });
   return lorem.generateWords(n);
+};
+
+const generateParagraphs = (n) => {
+  return lorem.generateParagraphs(n);
 };
 
 module.exports = {
   generateSentence,
+  generateParagraphs,
 };
